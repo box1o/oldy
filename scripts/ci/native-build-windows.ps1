952936206c1d1dfde4cf5403d3fde92144afead4
Rusu Alexei <alexei.r.2002@gmail.com>
2026-08-09 18:59:04 +0300

    Configure Windows Freetype paths

--- scripts/ci/native-build-windows.ps1
+++ scripts/ci/native-build-windows.ps1
@@ -8,6 +8,9 @@ cmake -B build -S . `
   -DCMAKE_PREFIX_PATH="$env:DAWN_PREFIX;$env:VCPKG_ROOT/installed/x64-windows" `
   -DDawn_DIR="$env:Dawn_DIR" `
   -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" `
+  -DFREETYPE_INCLUDE_DIR_ft2build="$env:VCPKG_ROOT/installed/x64-windows/include/freetype2" `
+  -DFREETYPE_INCLUDE_DIR_freetype2="$env:VCPKG_ROOT/installed/x64-windows/include/freetype2" `
+  -DFREETYPE_LIBRARY_RELEASE="$env:VCPKG_ROOT/installed/x64-windows/lib/freetype.lib" `
   -DVCPKG_TARGET_TRIPLET=x64-windows
 if ($LASTEXITCODE -ne 0) {
     throw "CMake configuration failed with exit code $LASTEXITCODE"
