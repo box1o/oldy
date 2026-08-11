74cc2ea9a2f16f33ff022d3090750f481ef10601
Rusu Alexei <alexei.r.2002@gmail.com>
2026-08-11 23:18:17 +0300

    Clean up Kitty extension tooling

--- extensions/kitty/src/plugin.cpp
+++ extensions/kitty/src/plugin.cpp
@@ -4,33 +4,41 @@
 
 namespace {
 
+using namespace woki;
+using namespace math;
+
 class Kitty final {
 public:
-    woki::Status OnAttach() noexcept {
+    Status OnAttach() noexcept {
         // nf-md-cat U+F011B
         entity_ = registry_.Create();
         if (!entity_ || positions_.Emplace(entity_, Position{}) == nullptr)
-            return woki::Status::NoSpace();
+            return Status::NoSpace();
         return slog::Info("\xF3\xB0\x84\x9B kitty says hi!");
     }
 
-    void OnUpdate(woki::f64 delta_ms) noexcept {
+    void OnUpdate(f64 delta_ms) noexcept {
         if (Position* position = positions_.Get(entity_))
             position->value.x += static_cast<float>(delta_ms * 0.001);
     }
 
-    void OnEvent(woki::events::Event& event) noexcept {
-        woki::events::EventDispatcher dispatcher{event};
-        dispatcher.Dispatch<woki::events::KeyPressedEvent>([](woki::events::KeyPressedEvent key) noexcept { (void)slog::Info("kitty saw key press: ", key.key, " repeat ", key.repeat_count); });
-        dispatcher.Dispatch<woki::events::WindowResizedEvent>([](woki::events::WindowResizedEvent resized) noexcept { (void)slog::Info("kitty saw window resize: ", resized.width, 'x', resized.height); });
+    void OnEvent(events::Event& event) noexcept {
+        events::EventDispatcher dispatcher{event};
+        dispatcher.Dispatch<events::KeyPressedEvent>([](events::KeyPressedEvent key) noexcept { (void)slog::Info("kitty saw key press: ", key.key, " repeat ", key.repeat_count); });
+        dispatcher.Dispatch<events::WindowResizedEvent>([](events::WindowResizedEvent resized) noexcept { (void)slog::Info("kitty saw window resize: ", resized.width, 'x', resized.height); });
+        dispatcher.Dispatch<events::WindowMovedEvent>([](events::WindowMovedEvent moved) noexcept { (void)slog::Info("kitty saw window move: ", moved.x, 'x', moved.y); });
+        dispatcher.Dispatch<events::WindowClosedEvent>([](events::WindowClosedEvent closed) noexcept { (void)slog::Info("kitty saw window close"); });
+        dispatcher.Dispatch<events::WindowMinimizedEvent>([](events::WindowMinimizedEvent minimized) noexcept { (void)slog::Info("kitty saw window minimize: ", minimized.minimized); });
+        dispatcher.Dispatch<events::WindowMaximizedEvent>([](events::WindowMaximizedEvent maximized) noexcept { (void)slog::Info("kitty saw window maximize: ", maximized.maximized); });
+        dispatcher.Dispatch<events::WindowRestoredEvent>([](events::WindowRestoredEvent restored) noexcept { (void)slog::Info("kitty saw window restore"); });
     }
 
-    woki::Status OnCommand(const woki::extension::Command& command) noexcept {
+    Status OnCommand(const extension::Command& command) noexcept {
         if (command.Id() == "woki.kitty.pet")
             return slog::Info("\xF3\xB0\x84\x9B *purrr*");
         if (command.Id() == "woki.kitty.complex")
             return slog::Info("\xF3\xB0\x84\x9B complex command!");
-        return woki::Status::NotFound();
+        return Status::NotFound();
     }
 
     void OnDetach() noexcept {
@@ -40,12 +48,12 @@ public:
 
 private:
     struct Position final {
-        woki::math::vec2<float> value{};
+        vec2<float> value{};
     };
 
-    woki::guest::Registry<8> registry_;
-    woki::guest::ComponentPool<Position, 8> positions_;
-    woki::guest::Entity entity_;
+    guest::Registry<8> registry_;
+    guest::ComponentPool<Position, 8> positions_;
+    guest::Entity entity_;
 };
 
 } // namespace
